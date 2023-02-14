143d58fab57c9eea65f01029c9b1164798ba56a1
Tomas Kubo <kubot.dev@gmail.com>
2023-02-14 22:31:29 +0100

    about page rework, added accordion

--- js/about.js
+++ js/about.js
@@ -24,3 +24,26 @@ accContainer.addEventListener('click', (event) => {
     updateAccordion(accordion, height)
   }
 })
+
+window.addEventListener('load', (event) => {
+  const accordionItem = Array.from(document.querySelectorAll('.about__listItem'))
+
+  accordionItem.forEach((element) => {
+    if (element.classList.contains('is-open')) {
+      const getContentHeight = (accordion) => {
+        const accordionInner = element.querySelector('.about__listItemText')
+        if (accordion.classList.contains('is-open')) return 0
+        return accordionInner.getBoundingClientRect().height
+      }
+      const updateAccordion = (accordion, height) => {
+        const accordionContent = accordion.querySelector('.accordion__content')
+        console.log(accordionContent)
+        accordionContent.style.height = `${height}px`
+      }
+      const accordion = element.parentElement
+      const height = getContentHeight(accordion)
+      updateAccordion(element, height)
+      console.log(accordion, height)
+    }
+  })
+})
