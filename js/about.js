e8a1b3aebf5a596d06e6500702632c65a72e06fd
Tomas Kubo <kubot.dev@gmail.com>
2023-07-18 14:18:47 +0200

    rework of design,about page, other minor changes

--- js/about.js
+++ js/about.js
@@ -1,52 +1,52 @@
 //
 
-const accordions = Array.from(document.querySelectorAll('.about__listItem'))
-const accContainer = document.querySelector('.about__aboutList')
+const accordions = Array.from(document.querySelectorAll('.about__listItem'));
+const accContainer = document.querySelector('.about__aboutList');
 
 accContainer.addEventListener('click', (event) => {
-  const accordionHeader = event.target.closest('.about__listItemHeading')
+  const accordionHeader = event.target.closest('.about__listItemHeading');
 
-  if (!accordionHeader) return
+  if (!accordionHeader) return;
   {
     const getContentHeight = (accordion) => {
-      const accordionInner = accordion.querySelector('.about__listItemText')
-      if (accordion.classList.contains('is-open')) return 0
-      return accordionInner.getBoundingClientRect().height
-    }
+      const accordionInner = accordion.querySelector('.about__listItemText');
+      if (accordion.classList.contains('is-open')) return 0;
+      return accordionInner.getBoundingClientRect().height;
+    };
     const updateAccordion = (accordion, height) => {
-      const accordionContent = accordion.querySelector('.accordion__content')
+      const accordionContent = accordion.querySelector('.accordion__content');
 
-      accordion.classList.toggle('is-open')
-      accordionContent.style.height = `${height}px`
+      accordion.classList.toggle('is-open');
+      accordionContent.style.height = `${height}px`;
 
-      accordionHeader.classList.toggle('rotated')
-    }
-    const accordion = accordionHeader.parentElement
-    const height = getContentHeight(accordion)
-    updateAccordion(accordion, height)
+      accordionHeader.classList.toggle('rotated');
+    };
+    const accordion = accordionHeader.parentElement;
+    const height = getContentHeight(accordion);
+    updateAccordion(accordion, height);
   }
-})
+});
 
 window.addEventListener('load', (event) => {
-  const accordionItem = Array.from(document.querySelectorAll('.about__listItem'))
+  const accordionItem = Array.from(document.querySelectorAll('.about__listItem'));
 
   accordionItem.forEach((element) => {
     if (element.classList.contains('is-open')) {
       const getContentHeight = (accordion) => {
-        const accordionInner = element.querySelector('.about__listItemText')
-        if (accordion.classList.contains('is-open')) return 0
-        return accordionInner.getBoundingClientRect().height
-      }
+        const accordionInner = element.querySelector('.about__listItemText');
+        if (accordion.classList.contains('is-open')) return 0;
+        return accordionInner.getBoundingClientRect().height;
+      };
       const updateAccordion = (accordion, height) => {
-        const accordionContent = accordion.querySelector('.accordion__content')
-        accordionContent.style.height = `${height}px`
-      }
-      const accordion = element.parentElement
-      const height = getContentHeight(accordion)
-      updateAccordion(element, height)
+        const accordionContent = accordion.querySelector('.accordion__content');
+        accordionContent.style.height = `${height}px`;
+      };
+      const accordion = element.parentElement;
+      const height = getContentHeight(accordion);
+      updateAccordion(element, height);
 
-      const heading = element.querySelector('.about__listItemHeading')
-      heading.classList.add('rotated')
+      const heading = element.querySelector('.about__listItemHeading');
+      heading.classList.add('rotated');
     }
-  })
-})
+  });
+});
--- js/design.js
+++ js/design.js
@@ -1,22 +1,154 @@
 // design js
-const design = document.querySelector('.design')
-const tabs = document.querySelector('.design__tabs').children
-const tabsArray = Array.from(tabs)
-const tabContent = Array.from(document.querySelectorAll('.tab-content'))
+const design = document.querySelector('.design');
+const tabs = document.querySelector('.design__tabs').children;
+const tabsArray = Array.from(tabs);
+const tabContent = Array.from(document.querySelectorAll('.tab-content'));
 
 tabsArray.forEach((tab) => {
   tab.addEventListener('click', (event) => {
-    const target = tab.dataset.tab
-    const targetContent = design.querySelectorAll(`[data-tab=${target}]`)[1]
+    const target = tab.dataset.tab;
+    const targetContent = design.querySelectorAll(`[data-tab=${target}]`)[1];
 
     tabsArray.forEach((t) => {
-      t.classList.remove('is-selected')
-    })
-    tab.classList.add('is-selected')
+      t.classList.remove('is-selected');
+    });
+    tab.classList.add('is-selected');
 
     tabContent.forEach((c) => {
-      c.classList.remove('is-selected')
-    })
-    targetContent.classList.add('is-selected')
-  })
-})
+      c.classList.remove('is-selected');
+    });
+    targetContent.classList.add('is-selected');
+  });
+});
+
+const modalOverlay = Array.from(document.querySelectorAll('.modal-overlay'));
+// const lpr1 = document.querySelector('.lpr1')
+// const lpr2 = document.querySelector('.lpr2')
+// const lpr3 = document.querySelector('.lpr3')
+const fd1 = document.querySelector('.fd1');
+const fd2 = document.querySelector('.fd2');
+const fd3 = document.querySelector('.fd3');
+// const bp1 = document.querySelector('.bp1')
+// const bp2 = document.querySelector('.bp2')
+// const bp3 = document.querySelector('.bp3')
+
+// lpr1.addEventListener('click', (event) => {
+//   lpr1.classList.add('modal-is-open')
+// })
+
+// modalOverlay[0].addEventListener('click', (event) => {
+//   event.stopPropagation()
+//   if (event.target.parentElement.classList.contains('designProject')) {
+//     lpr1.classList.remove('modal-is-open')
+//   }
+// })
+
+// lpr2.addEventListener('click', (event) => {
+//   lpr2.classList.add('modal-is-open')
+// })
+
+// modalOverlay[1].addEventListener('click', (event) => {
+//   event.stopPropagation()
+//   if (event.target.parentElement.classList.contains('designProject')) {
+//     lpr2.classList.remove('modal-is-open')
+//   }
+// })
+
+// lpr3.addEventListener('click', (event) => {
+//   lpr3.classList.add('modal-is-open')
+// })
+
+// modalOverlay[2].addEventListener('click', (event) => {
+//   event.stopPropagation()
+//   if (event.target.parentElement.classList.contains('designProject')) {
+//     lpr3.classList.remove('modal-is-open')
+//   }
+// })
+
+fd1.addEventListener('click', (event) => {
+  fd1.classList.add('modal-is-open');
+});
+
+modalOverlay[0].addEventListener('click', (event) => {
+  event.stopPropagation();
+  if (event.target.parentElement.classList.contains('designProject')) {
+    fd1.classList.remove('modal-is-open');
+  }
+});
+
+fd2.addEventListener('click', (event) => {
+  fd2.classList.add('modal-is-open');
+});
+
+modalOverlay[1].addEventListener('click', (event) => {
+  event.stopPropagation();
+  if (event.target.parentElement.classList.contains('designProject')) {
+    fd2.classList.remove('modal-is-open');
+  }
+});
+
+fd3.addEventListener('click', (event) => {
+  fd3.classList.add('modal-is-open');
+});
+
+modalOverlay[2].addEventListener('click', (event) => {
+  event.stopPropagation();
+  if (event.target.parentElement.classList.contains('designProject')) {
+    fd3.classList.remove('modal-is-open');
+  }
+});
+
+// bp1.addEventListener('click', (event) => {
+//   bp1.classList.add('modal-is-open');
+// });
+
+// modalOverlay[6].addEventListener('click', (event) => {
+//   event.stopPropagation();
+//   if (event.target.parentElement.classList.contains('designProject')) {
+//     bp1.classList.remove('modal-is-open');
+//   }
+// });
+
+// bp2.addEventListener('click', (event) => {
+//   bp2.classList.add('modal-is-open');
+// });
+
+// modalOverlay[7].addEventListener('click', (event) => {
+//   event.stopPropagation();
+//   if (event.target.parentElement.classList.contains('designProject')) {
+//     bp2.classList.remove('modal-is-open');
+//   }
+// });
+
+// bp3.addEventListener('click', (event) => {
+//   bp3.classList.add('modal-is-open');
+// });
+
+// modalOverlay[8].addEventListener('click', (event) => {
+//   event.stopPropagation();
+//   if (event.target.parentElement.classList.contains('designProject')) {
+//     bp3.classList.remove('modal-is-open');
+//   }
+// });
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.keyCode === 27) {
+    fd1.classList.remove('modal-is-open');
+  }
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.keyCode === 27) {
+    fd2.classList.remove('modal-is-open');
+  }
+});
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.keyCode === 27) {
+    fd3.classList.remove('modal-is-open');
+  }
+});
+
+// Cocoen.create(document.querySelector('.cocoen'))
+
+Cocoen.parse(document.body);
